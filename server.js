ae1db89beae95ef8338da2fd28785a4a716a6e4f
halimasoulb <47951591+halimasoulb@users.noreply.github.com>
2020-06-02 22:15:14 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/data-amine'));
+app.use(express.static(__dirname + '/dist/app-client-front'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/data-amine/index.html'));
+res.sendFile(path.join(__dirname+'/dist/app-client-front/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
